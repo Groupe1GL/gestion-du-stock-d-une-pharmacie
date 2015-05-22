2e5e51bb011656e1047a46ae3a4a4cd42d22f5d8
ForumGL <etudinatsgl@hotmail.fr>
2015-05-23 00:32:28 +0200

    Create stock.php

---
+++ stock.php
@@ -0,0 +1,55 @@
+# gestion-du-stock-d-une-pharmacie
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Bienvenue </title>
+</head>
+<body>
+	<div class="header">
+		<div class="ster"> <!-- <a href="sinscrir.html"><font size="+1">S'inscrire</font></a>  <font size="+1">||</font> -->
+		<a href="#"><font size="+1">Gestion de stock du pharmacie</font>
+		</a> <font size="+2">||</font>
+		
+		</a> 
+		</div>
+<!-- 		<div class="login">
+	<form method="post" action="">
+		<input class="input" type="text" placeholder="Enter Your Username" name="login"><br>
+		<input class="input" type="password"placeholder="Enter Your Password" name="pass"><br>
+		<input class="classname" type="submit" value="Se connecter">
+	</form>
+</div> -->
+		<div class="logo"><a href="home.html"><img src="lib/logo.png" height= 100% width= 100%> </a> </div>
+	</div>
+	<div class="cat">
+		<ul class="caul">
+			<li><a href="index.html"> Accueil <a/></li>
+			<li><a href="produits.php"> Produits <a/></li>
+			<li><a href="operations.php"> Opérations <a/></li>
+			<li><a href="stock.php">  <font color="#0f0"> Stock </font><a/></li>
+			
+		</ul>
+	</div>
+	<div class="photo"><img src="lib/tiro.png"	height= 25% width= 100%></div>
+		<div class="nouveaute">
+			
+			
+<div class="nom">Etat d'inventaire du stock par catégorie </div>
+<fieldset><legend>Choisir la catégorie : </legend>
+<form action="stock.php" method="post">
+<select name="catR">
+	<option value="SIROPS">SIROPS</option>
+	<option value="COMPRIME">COMPRIME</option>
+	<option value="COMPRIMES ATB">COMPRIMES ATB</option>
+	<option value="SACHETS">SACHETS</option>
+</select>
+<input type="submit" value="Valider">
+</form>
+</fieldset> 
+
+			<div class="stesr">
+				:: Etudiantes : Mariam ELHAMIDI , Ouiam ANTAM  :: GL S4 ::
+		</div>
+</body>
+</html>
